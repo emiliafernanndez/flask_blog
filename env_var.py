43a0f476908058e1cdbb3084d6e77487abfb6f93
emiliafernanndez <11emiliafernandez@gmail.com>
2025-01-21 11:21:46 -0300

    Proyecto finalizado

---
+++ env_var.py
@@ -0,0 +1,6 @@
+import os
+
+EMAIL_USER = os.environ.get('EMAIL_USER')
+EMAIL_PASS = os.environ.get('EMAIL_PASS')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
